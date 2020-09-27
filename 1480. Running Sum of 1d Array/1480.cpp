42c44944b6527b3a13e2aa79363250365ebc068d
얌챠 <dbals9812@naver.com>
2020-09-27 15:33:33 +0900

    add solution of 1480 add gitignore

---
+++ 1480. Running Sum of 1d Array/1480.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> res(nums.size());
+        res[0] = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            res[i] = res[i - 1] + nums[i];
+        }
+
+        return res;
+    }
+};
+
+int main(void) {
+    Solution sol = Solution();
+    vector<int> v = { 1,2,3,4 };
+    sol.runningSum(v);
+    return 0;
+}
