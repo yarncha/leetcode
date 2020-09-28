2bc17d14484d121d277b475b998fc5f74172a688
얌챠 <dbals9812@naver.com>
2020-09-28 19:45:16 +0900

    add solution of problem 344

---
+++ 344. Reverse String/344.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	void reverseString(vector<char>& s) {
+		char temp;
+		int count = 0;
+		while (count< (int)(s.size() - 1 - count)) {
+			temp = s[count];
+			s[count] = s[s.size() -1 - count];
+			s[s.size() - 1 - count] = temp;
+			count++;
+		}
+	}
+};
+
+int main(void) {
+	Solution sol = Solution();
+	vector<char> v = { };
+	vector<char> v22 = { 'h', 'e', 'l', 'l', 'o' };
+
+	sol.reverseString(v);
+	sol.reverseString(v22);
+	return 0;
+}
