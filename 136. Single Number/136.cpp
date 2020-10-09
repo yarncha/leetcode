7517a27e23fdce17754bd3fda2f8191e0b5df69a
얌챠 <dbals9812@naver.com>
2020-10-09 22:00:55 +0900

    ★ add solution of problem 136

--- 136. Single Number/136.cpp
+++ 136. Single Number/136.cpp
@@ -3,11 +3,23 @@
 using namespace std;
 
 class Solution {
-	public int singleNumber(int[] nums) {
+public:
+	int singleNumber(vector<int>& nums) {
+		int res = nums[0];
 
+		for (int i = 1; i < nums.size(); i++)
+		{
+			res ^= nums[i];
+		}
+
+		return res;
 	}
-}
+};
 
 int main(void) {
+	Solution sol = Solution();
+	vector<int> nums = { 4,2,1,2,1 };
+
+	int a = sol.singleNumber(nums);
 	return 0;
 }
