68b90cae9574c43228e95cc79a6904024f95f225
얌챠 <dbals9812@naver.com>
2020-10-20 22:28:48 +0900

    add solution of problem 206

---
+++ 206. Reverse Linked List/206.cpp
@@ -0,0 +1,30 @@
+﻿struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+	// reverse ListNode iteratively
+	ListNode* reverseList(ListNode* head) {
+		ListNode* current_node = head;
+
+		// 주어지는 head 노드가 nullptr일 경우도 있어서 해당 경우에 대해서 처리해 주어야 한다.
+		ListNode* result_node = nullptr;
+		if (current_node != nullptr)
+		{
+			result_node = new ListNode(current_node->val);
+
+			while (current_node->next != nullptr)
+			{
+				current_node = current_node->next;
+				result_node = new ListNode(current_node->val, result_node);
+			}
+		}
+
+		return result_node;
+	}
+};
