93270adecf080e472806934ebd89f4e1275c810d
얌챠 <dbals9812@naver.com>
2020-10-21 23:45:04 +0900

    Create 206_recursive.cpp

---
+++ 206. Reverse Linked List/206_recursive.cpp
@@ -0,0 +1,13 @@
+﻿struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+	// reverse ListNode recursively
+	}
+};
