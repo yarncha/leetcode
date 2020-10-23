63c810373ac737988cf724458cbf36b37ec5db16
얌챠 <dbals9812@naver.com>
2020-10-23 23:44:09 +0900

    Update 206_recursive.cpp

--- 206. Reverse Linked List/206_recursive.cpp
+++ 206. Reverse Linked List/206_recursive.cpp
@@ -9,5 +9,21 @@
 class Solution {
 public:
 	// reverse ListNode recursively
+		ListNode* current_node = head;
+
+		// 주어지는 head 노드가 nullptr일 경우도 있어서 해당 경우에 대해서 처리해 주어야 한다.
+		ListNode* result_node = nullptr;
+		if (current_node != nullptr)
+		{
+			result_node = new ListNode(current_node->val);
+
+			while (current_node->next != nullptr)
+			{
+				current_node = current_node->next;
+				result_node = new ListNode(current_node->val, result_node);
+			}
+		}
+
+		return result_node;
 	}
 };
