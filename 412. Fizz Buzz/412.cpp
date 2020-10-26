9032130098fbc381eef75378557272b435f93f87
얌챠 <dbals9812@naver.com>
2020-10-26 18:47:45 +0900

    add solution of problem 412

--- 412. Fizz Buzz/412.cpp
+++ 412. Fizz Buzz/412.cpp
@@ -6,11 +6,32 @@ using namespace std;
 class Solution {
 public:
 	vector<string> fizzBuzz(int n) {
+		vector<string> res;
 
+		for (int i = 1; i < n+1; i++)
+		{
+			if (i % 15 == 0) {
+				res.push_back("FizzBuzz");
+			}
+			else if(i%5==0)
+			{
+				res.push_back("Buzz");
+			}
+			else if (i % 3 == 0)
+			{
+				res.push_back("Fizz");
+			}
+			else {
+				res.push_back(to_string(i));
+			}
+		}
+
+		return res;
 	}
 };
 
 int main(void) {
 	Solution sol = Solution();
+	vector<string> a = sol.fizzBuzz(17);
 	return 0;
 }
