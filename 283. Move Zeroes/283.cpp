b4f426069d4d454219732620e9f947301854eb09
얌챠 <dbals9812@naver.com>
2020-10-27 21:36:03 +0900

    add solution of problem 283

---
+++ 283. Move Zeroes/283.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	void moveZeroes(vector<int>& nums) {
+
+	}
+};
+
+int main(void) {
+	Solution sol = Solution();
+	vector<int> a = { 1,2,0,3,0,0,4,5 };
+	sol.moveZeroes(a);
+	return 0;
+}
