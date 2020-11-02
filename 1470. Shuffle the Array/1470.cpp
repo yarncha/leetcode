1c15b7503c7528008a81d88fd011fdeb8bcd45f8
얌챠 <dbals9812@naver.com>
2020-11-02 18:17:36 +0900

    100% memory solution

---
+++ 1470. Shuffle the Array/1470.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> shuffle(vector<int>& nums, int n) {
+		int location = n;
+		while (location<2*n)
+		{
+			nums.insert(nums.begin()+(2 * (location - n + 1) - 1), nums[location]);
+			location++;
+			nums.erase(nums.begin()+location);
+		}
+		return nums;
+	}
+};
+
+int main(void) {
+	Solution sol = Solution();
+	vector<int> nums = { 1,2,3,4,4,3,2,1 };
+	vector<int> a = sol.shuffle(nums,4);
+	return 0;
+}
