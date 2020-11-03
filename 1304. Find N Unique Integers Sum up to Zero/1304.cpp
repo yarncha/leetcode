88ef223895418ac11a5fed032b64d89d286ea960
얌챠 <dbals9812@naver.com>
2020-11-03 20:17:18 +0900

    add

---
+++ 1304. Find N Unique Integers Sum up to Zero/1304.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> sumZero(int n) {
+
+	}
+};
+
+int main(void) {
+	Solution sol = Solution();
+	vector<int> a = sol.sumZero(6);
+	return 0;
+}
