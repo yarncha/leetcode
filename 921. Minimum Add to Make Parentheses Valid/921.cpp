0473053942595e7ebfa0bd2c8b368bc079994856
얌챠 <dbals9812@naver.com>
2020-11-24 16:57:12 +0900

    [stack] parentheses matching

---
+++ 921. Minimum Add to Make Parentheses Valid/921.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <vector>
+#include <string>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+	int minAddToMakeValid(string S) {
+
+	}
+};
+
+int main(void) {
+	Solution sol = Solution();
+	string input = "qwer";
+	sol.minAddToMakeValid(input);
+	return 0;
+}
