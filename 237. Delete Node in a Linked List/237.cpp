ee8dc49723e57efecc109b547f3831ead15967d6
얌챠 <dbals9812@naver.com>
2020-11-24 17:03:37 +0900

    simple programming

--- 237. Delete Node in a Linked List/237.cpp
+++ 237. Delete Node in a Linked List/237.cpp
@@ -1,8 +1,8 @@
-class Solution {
+癤풻lass Solution {
 public:
-    void deleteNode(ListNode* node) {
-        // 지울 노드가 항상 마지막 노드(tail node)가 아니라고 했으니, 그냥 다음 노드의 val과 next로 바꿔줌
-        node->val = (node->next)->val;
-        node->next = (node->next)->next;
-    }
+	void deleteNode(ListNode* node) {
+		// because node to delete is always not tail node, just change current node's value and next to next node's
+		node->val = (node->next)->val;
+		node->next = (node->next)->next;
+	}
 };
