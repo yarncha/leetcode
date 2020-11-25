6810af66d9d50294d3f9414172d17355013f3197
얌챠 <dbals9812@naver.com>
2020-11-25 15:54:58 +0900

    [stack] parentheses matching (100% Runtime)

--- 921. Minimum Add to Make Parentheses Valid/921.cpp
+++ 921. Minimum Add to Make Parentheses Valid/921.cpp
@@ -7,7 +7,28 @@ using namespace std;
 class Solution {
 public:
 	int minAddToMakeValid(string S) {
+		int result = 0;
 
+		stack<char> s;	//stack to save '('
+
+		for (int i = 0; i < S.size(); i++)
+		{
+			if (S[i] == '(') {
+				s.push('(');
+			}
+			else {
+				if (s.empty()) {
+					result++;
+				}
+				else {
+					s.pop();
+				}
+			}
+		}
+
+		result = result + s.size();
+
+		return result;
 	}
 };
 
