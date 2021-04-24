68f2c044fbba039cb260d4ccec4f94ebff3ad8dc
YAMCHA <dbals9812@naver.com>
2021-04-24 22:53:22 +0900

    add other files

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void Solution0() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	//ios_base::sync_with_stdio(false);
+	//cin.tie(nullptr);
+	//cout.tie(nullptr);
+
+	int a, b;
+	cin >> a >> b;
+	cout << "qwer";
+}
+
+//int main(void) {
+//	Solution0();
+//	return 0;
+//}
